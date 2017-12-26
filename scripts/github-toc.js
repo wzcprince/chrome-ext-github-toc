c53424dc5e64b895a30a7bc807d691cb951c9bb4
wzcprince <wzcprince@gmail.com>
2017-12-26 23:03:04 +0800

    更顺眼

--- scripts/github-toc.js
+++ scripts/github-toc.js
@@ -7,26 +7,18 @@
     $('article.markdown-body').addClass('toc-mian-body')
 
     $('#toc').toc({
-      'selectors': 'h1,h2,h3', //elements to use as headings
+      'selectors': 'h1,h2,h3,h4,h5,h6', //elements to use as headings
       'container': '.markdown-body.toc-mian-body', //element to find all selectors in
       'smoothScrolling': true, //enable or disable smooth scrolling on click
       'prefix': 'toc', //prefix for anchor tags and class names
       'onHighlight': function(el) {}, //called when a new section is highlighted 
       'highlightOnScroll': true, //add class to heading that is currently in focus
-      'highlightOffset': 100, //offset to trigger the next headline
-      'anchorName': function(i, heading, prefix) { //custom function for anchor name
-          return prefix+i;
-      },
+      'highlightOffset': 500, //offset to trigger the next headline
+      
       'headerText': function(i, heading, $heading) { //custom function building the header-item text
           var rank = $heading.prop("tagName").substr(1)
-          if ( rank > 2 ) {
-            return '- ' + $heading.text();
-          } 
-          return $heading.text();
+          return "¡ñ"/* chinese huge dot */ + $heading.text(); 
       },
-      // 'itemClass': function(i, heading, $heading, prefix) { // custom function for item class
-      //   return prefix + '-' +$heading[0].tagName.toLowerCase();
-      // }
     });
 
     $('.markdown-body.toc-mian-body').prepend("<h2>Table of Contents</h2>");
